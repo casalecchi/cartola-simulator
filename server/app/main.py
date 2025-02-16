63146b663133343f9e41cb191b9a7a2ba146c653
casalecchi <felipevilela191@gmail.com>
2025-02-16 11:45:08 -0300

    add CORS permission on FastAPI

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routers import api
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permitir apenas o frontend (React/Vue/Angular)
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Métodos permitidos
+    allow_headers=["*"],  # Permitir todos os headers
+)
 app.include_router(api.router, prefix="/api")
 
 
