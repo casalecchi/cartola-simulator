49800c31ed3c3bb7925a1de8b70ea1314d42660d
casalecchi <felipevilela191@gmail.com>
2025-02-16 11:18:16 -0300

    configure properly eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,24 +1,25 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config(
     { ignores: ['dist'] },
     {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
         files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
         languageOptions: {
             ecmaVersion: 2023,
             globals: globals.browser,
         },
         plugins: {
+            react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
         },
-        prettierConfig,
         rules: {
             ...reactHooks.configs.recommended.rules,
             'react/react-in-jsx-scope': 0,
