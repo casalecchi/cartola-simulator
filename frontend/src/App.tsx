3e58da34b8527804d75d150614e302a6b01a5a16
casalecchi <felipevilela191@gmail.com>
2025-02-15 22:47:29 -0300

    test translation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import { darkTheme } from './configurations/theme'
 
 const App: FC = () => {
+    const { t } = useTranslation()
+
     return (
         <ThemeProvider theme={darkTheme}>
             <CssBaseline>
-                <>Teste</>
+                <>{t('test')}</>
             </CssBaseline>
         </ThemeProvider>
     )
