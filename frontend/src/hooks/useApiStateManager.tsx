32701e9a2a380f72c01e392a7af8b7b6a526402f
casalecchi <felipevilela191@gmail.com>
2025-03-02 11:43:42 -0300

    start removing states from Context - still work to be done

--- frontend/src/hooks/useApiStateManager.tsx
+++ frontend/src/hooks/useApiStateManager.tsx
@@ -1,24 +1,16 @@
 import { useEffect, useState } from 'react'
 import { PlayerInfo } from '../models/player'
 import { useGetPlayersFromYear } from './useGetPlayersFromYear'
-import { useGetDataYears } from './useGetYears'
 
 export interface ApiStateManager {
     playersInfo: PlayerInfo[]
     selectedYear?: string
     setSelectedYear: (value: string) => void
-    years: number[]
 }
 
 export const useApiStateManager = (): ApiStateManager => {
     const [selectedYear, setSelectedYear] = useState('')
-    const { getDataYears, years } = useGetDataYears()
     const { fetchPlayersInfo, playersInfo } = useGetPlayersFromYear()
-
-    useEffect(() => {
-        getDataYears()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
 
     useEffect(() => {
         if (selectedYear == '') return
@@ -27,5 +19,5 @@ export const useApiStateManager = (): ApiStateManager => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [selectedYear])
 
-    return { playersInfo, selectedYear, setSelectedYear, years }
+    return { playersInfo, selectedYear, setSelectedYear }
 }
