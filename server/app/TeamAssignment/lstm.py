3e05a30b8514fb0c8dc2ad7fb83b0dc218e86e35
casalecchi <felipevilela191@gmail.com>
2025-03-28 23:12:40 -0300

    create LSTM methods and full simulation

--- server/app/TeamAssignment/lstm.py
+++ server/app/TeamAssignment/lstm.py
@@ -1,13 +1,22 @@
+import json
 import pandas as pd
 import numpy as np
 import os
+import warnings
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from keras.callbacks import EarlyStopping
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
-from typing import Dict
+from typing import Dict, List, Tuple
 from utils.dir import load_all_csvs
+from utils.players import get_players_from_season
+
+
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 # Função para criar sequências de entrada e saída
@@ -33,10 +42,8 @@ def player_lstm(
     player_id: int,
     previous_csvs: Dict[str, pd.DataFrame],
     next_csvs: Dict[str, pd.DataFrame],
+    n_steps: int = 5,
 ):
-    # Definir o número de rodadas (passos anteriores) usados para prever a próxima
-    n_steps = 10  # Ajustar conforme necessário
-
     predictions = []
 
     prev_timeseries = get_timeseries(player_id, previous_csvs)
@@ -45,7 +52,7 @@ def player_lstm(
 
     if np.count_nonzero(~np.isnan(prev_timeseries)) < n_steps:
         print(f"Jogador {player_id} não tem dados suficientes para criar sequências.")
-        return  # Pular jogadores que não têm dados suficientes
+        return [0.0 for _ in range(38)]
 
     context = np.nan_to_num(prev_timeseries)
     scaler = StandardScaler()
@@ -62,16 +69,18 @@ def player_lstm(
 
     # --- DEFINIÇÃO DO MODELO ---
     model = Sequential()
-    model.add(LSTM(32, input_shape=(n_steps, 1)))
+    model.add(LSTM(64, return_sequences=True, input_shape=(n_steps, 1), dropout=0.2))
+    model.add(LSTM(32))
     model.add(Dense(1))
-    model.compile(optimizer="adam", loss="mean_squared_error")
-    early_stop = EarlyStopping(monitor="loss", patience=5, restore_best_weights=True)
-    model.fit(X_train, y_train, epochs=30, batch_size=4, callbacks=[early_stop], verbose=0)
+
+    model.compile(optimizer="adam", loss="mae")
+    early_stop = EarlyStopping(monitor="loss", patience=20, restore_best_weights=True)
+    model.fit(X_train, y_train, epochs=500, batch_size=1, callbacks=[early_stop], verbose=0)
 
     # Começa com as últimas n_steps da temporada passada
     current_sequence = scaled_prev[-n_steps:].reshape(1, n_steps, 1)
 
-    for r in tqdm(range(38)):
+    for r in range(38):
         pred = model.predict(current_sequence, verbose=0)
         prediction = scaler.inverse_transform(pred)[0, 0]
         predictions.append(prediction)
@@ -83,24 +92,70 @@ def player_lstm(
             current_sequence[:, 1:, :], scaled_real.reshape(1, 1, 1), axis=1
         )
 
-    print(predictions)
-    return predictions
+    return [float(pred) for pred in predictions]
+
+
+def all_players_lstm(
+    year: int,
+    previous_csvs: Dict[str, pd.DataFrame],
+    next_csvs: Dict[str, pd.DataFrame],
+    players: Dict[int, Tuple[str, str, int]],
+    n_steps=5,
+    num_workers=0,
+):
+    all_predictions: Dict[int, List[float]] = {}
+
+    if num_workers > 0:
+        with ProcessPoolExecutor(max_workers=num_workers) as executor:
+            futures = {
+                executor.submit(player_lstm, id, previous_csvs, next_csvs, n_steps): id
+                for id in players.keys()
+            }
+
+            for future in tqdm(
+                as_completed(futures), total=len(futures), desc="Processando jogadores"
+            ):
+                try:
+                    key = futures[future]
+                    all_predictions[key] = future.result()
+                except Exception as e:
+                    print(f"Erro no future loop. {e}")
+    else:
+        for id in tqdm(players.keys()):
+            pred = player_lstm(id, previous_csvs, next_csvs, n_steps)
+            all_predictions[id] = pred
+
+    ROOT_DIR = Path(__file__).resolve().parent.parent
+    path = os.path.join(ROOT_DIR, f"static/lstm/{year}-{n_steps}.json")
+    with open(path, "w") as f:
+        json.dump(all_predictions, f, indent=4, ensure_ascii=False)
+    return all_predictions
+
+
+def run_lstm(year: int, previous_path: str, next_path: str, n_steps: int = 5, num_workers=0):
+    prev_csv = load_all_csvs(previous_path)
+    next_csv = load_all_csvs(next_path)
+    players = get_players_from_season(next_path)
+    all_players_lstm(year, prev_csv, next_csv, players, n_steps, num_workers)
 
 
 if __name__ == "__main__":
     data_dir = os.path.join(os.path.dirname(__file__), "data/")
     path_2019 = os.path.join(data_dir, "2019")
     path_2020 = os.path.join(data_dir, "2020")
-    prev_csv = load_all_csvs(path_2019)
-    next_csv = load_all_csvs(path_2020)
-    preds = player_lstm(42500, prev_csv, next_csv)
-    reais = get_timeseries(42500, next_csv)
+    run_lstm(2020, path_2019, path_2020, num_workers=10)
 
-    import matplotlib.pyplot as plt
+    # Código para teste rápido do modelo
+    # prev_csv = load_all_csvs(path_2019)
+    # next_csv = load_all_csvs(path_2020)
+    # preds = player_lstm(83257, prev_csv, next_csv)
+    # reais = get_timeseries(83257, next_csv)
 
-    plt.plot(reais, label="Real")
-    plt.plot(preds, label="Previsto")
-    plt.title("Previsão rodada a rodada")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # import matplotlib.pyplot as plt
+
+    # plt.plot(reais, label="Real")
+    # plt.plot(preds, label="Previsto")
+    # plt.title("Previsão rodada a rodada")
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
