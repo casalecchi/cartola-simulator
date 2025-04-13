4df8b9cc38857f50f292d08d57cfd1614d395d8b
casalecchi <felipevilela191@gmail.com>
2025-04-13 09:02:25 -0300

    adjust for all players

--- server/app/TeamAssignment/lstm.py
+++ server/app/TeamAssignment/lstm.py
@@ -1,5 +1,4 @@
 import json
-import pandas as pd
 import numpy as np
 import os
 import warnings
@@ -25,29 +24,48 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 class LSTMHyperModel(HyperModel):
-    def __init__(self, input_shape):
-        self.input_shape = input_shape
+    def __init__(self, scaled_prev, build_data_fn):
+        self.scaled_prev = scaled_prev
+        self.build_data = build_data_fn
 
     def build(self, hp):
+        n_steps = hp.Choice("n_steps", [2, 5, 10])
+        input_shape = (n_steps, 1)
+
         model = Sequential()
         model.add(
             LSTM(
-                units=hp.Int("units_1", 64, 416, step=32),
+                units=hp.Choice("units_1", [64, 128, 256]),
                 return_sequences=True,
-                input_shape=self.input_shape,
+                input_shape=input_shape,
             )
         )
         model.add(
             LSTM(
-                units=hp.Int("units_2", 32, 160, step=16),
+                units=hp.Choice("units_2", [32, 64]),
             )
         )
         model.add(Dense(1))
 
-        learning_rate = hp.Float("learning_rate", 1e-4, 1e-2, sampling="log")
-        optimizer = optimizers.Adam(learning_rate=learning_rate)
+        optimizer = optimizers.Adam(learning_rate=0.005)
         model.compile(optimizer=optimizer, loss="mae")
         return model
+
+    def fit(self, hp, model, *args, **kwargs):
+        n_steps = hp.get("n_steps")
+        epochs = 850  # hp.Choice("epochs", [750, 1000])
+
+        X_train, y_train = self.build_data(n_steps)
+
+        return model.fit(
+            X_train,
+            y_train,
+            validation_split=0.2,
+            epochs=epochs,
+            batch_size=4,
+            callbacks=[EarlyStopping(patience=20, restore_best_weights=True)],
+            verbose=0,
+        )
 
 
 # Função para criar sequências de entrada e saída
@@ -61,19 +79,20 @@ def create_sequences(data, n_steps):
 
 def player_lstm(
     player_id: int,
-    previous_csvs: Dict[str, pd.DataFrame],
-    next_csvs: Dict[str, pd.DataFrame],
-    n_steps=5,
+    previous_path: str,
+    next_path: str,
 ):
     predictions = []
+    previous_csvs = load_all_csvs(previous_path)
+    next_csvs = load_all_csvs(next_path)
 
     prev_timeseries = get_timeseries(player_id, previous_csvs)
     next_timeseries = get_timeseries(player_id, next_csvs)
     next_timeseries = np.nan_to_num(next_timeseries)
 
-    if np.count_nonzero(~np.isnan(prev_timeseries)) < n_steps:
+    if np.count_nonzero(np.nan_to_num(prev_timeseries)) < 15:
         print(f"Jogador {player_id} não tem dados suficientes para criar sequências.")
-        return [0.0 for _ in range(38)]
+        return [float("-inf") for _ in range(38)]
 
     context = np.nan_to_num(prev_timeseries)
     scaler = StandardScaler()
@@ -84,67 +103,38 @@ def player_lstm(
         X = X.reshape((X.shape[0], X.shape[1], 1))
         return X, y
 
-    # Hiperparâmetro tunável fora do modelo: n_steps
-    def model_builder(hp):
-        n_steps = hp.Int("n_steps", min_value=2, max_value=10, step=1)
-        epochs = hp.Int("epochs", min_value=100, max_value=1200, step=50)
-        X_train, y_train = build_data(n_steps)
-
-        model = LSTMHyperModel(input_shape=(n_steps, 1)).build(hp)
-
-        model.fit(
-            X_train,
-            y_train,
-            validation_split=0.2,
-            epochs=epochs,
-            batch_size=4,
-            callbacks=[EarlyStopping(patience=5)],
-            verbose=0,
-        )
-
-        return model
+    hypermodel = LSTMHyperModel(scaled_prev, build_data)
 
     tuner = RandomSearch(
-        model_builder,
+        hypermodel,
         objective="val_loss",
-        max_trials=30,
+        max_trials=18,
         executions_per_trial=1,
-        directory="lstm_tuning",
-        project_name=f"player_{player_id}",
+        directory=f"lstm_tuning/player_{player_id}",
+        project_name="run",
         overwrite=True,
     )
 
-    # Dummy data just to allow initial build (Keras Tuner requer isso no começo)
-    dummy_X, dummy_y = build_data(5)
-    tuner.search_space_summary()
-    tuner.search(
-        dummy_X, dummy_y, epochs=1, batch_size=1, validation_split=0.2, verbose=0
-    )  # inicia espaço
-    tuner.oracle.max_trials = 30  # definir max_trials depois de ajustar n_steps dinamicamente
-
-    tuner.search(dummy_X, dummy_y, verbose=0)  # real tuning
+    tuner.search(verbose=0)
 
     best_hp = tuner.get_best_hyperparameters(1)[0]
     best_n_steps = best_hp.get("n_steps")
-    best_epochs = best_hp.get("epochs")
+    # best_epochs = best_hp.get("epochs")
 
     X_train, y_train = build_data(best_n_steps)
-    print(f"Y: {y_train}")
-    print(f"prev: {prev_timeseries}")
-    print(f"scaled_prev: {scaled_prev}")
-    print("Best hyperparameters:", best_hp.values)
-    print("Exemplo de y_train:", y_train[:10])
-    print("Variância de y_train:", np.var(y_train))
 
-    model = LSTMHyperModel(input_shape=(best_n_steps, 1)).build(best_hp)
+    # Pega o melhores hiperparâmetros e re-treina o modelo
+    model = LSTMHyperModel(scaled_prev, build_data).build(best_hp)
     model.fit(
         X_train,
         y_train,
-        epochs=best_epochs,
+        epochs=850,
         batch_size=4,
-        callbacks=[EarlyStopping(patience=10)],
+        callbacks=[EarlyStopping(patience=20, restore_best_weights=True)],
         verbose=0,
     )
+    ROOT_DIR = Path(__file__).resolve().parent.parent
+    model.save(os.path.join(ROOT_DIR, f"keras_cache/attempt2/player_{player_id}.keras"))
 
     # Começa com as últimas n_steps da temporada passada
     current_sequence = scaled_prev[-best_n_steps:].reshape(1, best_n_steps, 1)
@@ -166,10 +156,9 @@ def player_lstm(
 
 def all_players_lstm(
     year: int,
-    previous_csvs: Dict[str, pd.DataFrame],
-    next_csvs: Dict[str, pd.DataFrame],
+    previous_path: str,
+    next_path: str,
     players: Dict[int, Tuple[str, str, int, str]],
-    n_steps=5,
     num_workers=0,
 ):
     all_predictions: Dict[int, List[float]] = {}
@@ -177,11 +166,12 @@ def all_players_lstm(
     total_mse = 0
     total_rmse = 0
     jogadores_validos = 0
+    next_csvs = load_all_csvs(next_path)
 
     if num_workers > 0:
         with ProcessPoolExecutor(max_workers=num_workers) as executor:
             futures = {
-                executor.submit(player_lstm, id, previous_csvs, next_csvs, n_steps): id
+                executor.submit(player_lstm, id, previous_path, next_path): id
                 for id in players.keys()
             }
 
@@ -191,9 +181,10 @@ def all_players_lstm(
                 try:
                     key = futures[future]
                     pred = future.result()
-                    all_predictions[key] = future.result()
+                    all_predictions[key] = pred
+
                     # Calcular MAE se for válido
-                    if pred != [0.0 for _ in range(38)] and len(pred) == 38:
+                    if pred != [float("-inf") for _ in range(38)] and len(pred) == 38:
                         real = get_timeseries(key, next_csvs)
                         real = np.nan_to_num(real)
 
@@ -209,7 +200,9 @@ def all_players_lstm(
                     print(f"Erro no future loop. {e}")
     else:
         for id in tqdm(players.keys()):
-            pred = player_lstm(id, previous_csvs, next_csvs, n_steps)
+            pred = player_lstm(id, previous_path, next_path)
+            if pred == []:
+                continue
             all_predictions[id] = pred
 
     print(f"Validos {jogadores_validos}")
@@ -221,36 +214,34 @@ def all_players_lstm(
     print(f"\nErro absoluto médio (RMSE) entre todos os jogadores com ARIMA: {avg_rmse:.2f}")
 
     ROOT_DIR = Path(__file__).resolve().parent.parent
-    path = os.path.join(ROOT_DIR, f"static/lstm/{year}-{n_steps}.json")
+    path = os.path.join(ROOT_DIR, f"static/lstm/{year}-tuner.json")
     with open(path, "w") as f:
         json.dump(all_predictions, f, indent=4, ensure_ascii=False)
     return all_predictions
 
 
-def run_lstm(year: int, previous_path: str, next_path: str, n_steps: int = 5, num_workers=0):
-    prev_csv = load_all_csvs(previous_path)
-    next_csv = load_all_csvs(next_path)
+def run_lstm(year: int, previous_path: str, next_path: str, num_workers=0):
     players = get_players_from_season(next_path)
-    all_players_lstm(year, prev_csv, next_csv, players, n_steps, num_workers)
+    all_players_lstm(year, previous_path, next_path, players, num_workers)
 
 
 if __name__ == "__main__":
     data_dir = os.path.join(os.path.dirname(__file__), "data/")
     path_2019 = os.path.join(data_dir, "2019")
     path_2020 = os.path.join(data_dir, "2020")
-    # run_lstm(2020, path_2019, path_2020, num_workers=10)
+    run_lstm(2020, path_2019, path_2020, num_workers=10)
 
     # Código para teste rápido do modelo 87863 83257 38162 38750
-    prev_csv = load_all_csvs(path_2019)
-    next_csv = load_all_csvs(path_2020)
-    preds = player_lstm(38162, prev_csv, next_csv)
-    reais = get_timeseries(38162, next_csv)
+    # prev_csv = load_all_csvs(path_2019)
+    # next_csv = load_all_csvs(path_2020)
+    # preds = player_lstm(38162, prev_csv, next_csv)
+    # reais = get_timeseries(38162, next_csv)
 
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
 
-    plt.plot(reais, label="Real")
-    plt.plot(preds, label="Previsto")
-    plt.title("Previsão rodada a rodada")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # plt.plot(reais, label="Real")
+    # plt.plot(preds, label="Previsto")
+    # plt.title("Previsão rodada a rodada")
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
