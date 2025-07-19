088dd5aab58977acd76e80005454e246857087c2
casalecchi <felipevilela191@gmail.com>
2025-07-19 18:03:45 -0300

    comment MobileRow - not been used yet

--- frontend/src/components/Market.tsx
+++ frontend/src/components/Market.tsx
@@ -177,79 +177,79 @@ const Row: FC<RowProps> = ({ balance, isOnTeam, player, teamInfo, buy, sell }) =
     )
 }
 
-const MobileRow: FC<RowProps> = ({ balance, isOnTeam, player, teamInfo, buy, sell }) => {
-    return (
-        <Stack alignItems={'center'} direction={'row'} spacing={1}>
-            <Avatar
-                alt={player.name}
-                src={player.photoUrl}
-                sx={{ height: 100, width: 100 }}
-                variant={'square'}
-            />
-            <Stack spacing={1} width={'100%'}>
-                <Stack alignItems={'center'} direction={'row'} spacing={2}>
-                    <StatusIcon status={player.statusId} />
-                    <Stack
-                        alignItems={'center'}
-                        direction={'row'}
-                        justifyContent={'space-between'}
-                        width={'100%'}
-                    >
-                        <Typography fontSize={'1.2rem'}>{player.name}</Typography>
-                        <Typography fontSize={'0.75rem'} sx={{ opacity: 0.4 }}>
-                            {teamInfo?.name}
-                        </Typography>
-                    </Stack>
-                </Stack>
-                <Stack
-                    alignItems={'center'}
-                    direction={'row'}
-                    justifyContent={'space-between'}
-                    pl={1}
-                >
-                    <Stack alignItems={'center'}>
-                        <Typography fontSize={'0.8rem'} sx={{ opacity: 0.4 }}>
-                            LAST
-                        </Typography>
-                        <Typography>{player.lastScore}</Typography>
-                    </Stack>
-                    <Stack alignItems={'center'}>
-                        <Typography fontSize={'0.8rem'} sx={{ opacity: 0.4 }}>
-                            AVG
-                        </Typography>
-                        <Typography>{player.average}</Typography>
-                    </Stack>
-                    <Stack alignItems={'center'}>
-                        <Typography fontSize={'0.8rem'} sx={{ opacity: 0.4 }}>
-                            GAMES
-                        </Typography>
-                        <Typography>{player.gamesPlayed}</Typography>
-                    </Stack>
-                </Stack>
-                <Stack
-                    alignItems={'center'}
-                    direction={'row'}
-                    justifyContent={'space-between'}
-                    pl={1}
-                >
-                    <Typography fontSize={'1.3rem'}>{`${t('common.cartoleta')}${
-                        player.price
-                    }`}</Typography>
-                    <Button
-                        disabled={balance - player.price < 0 && !isOnTeam}
-                        onClick={() => (isOnTeam ? sell(player) : buy(player))}
-                        sx={{
-                            color: 'white',
-                            backgroundColor: isOnTeam ? 'red' : 'green',
-                            borderRadius: 8,
-                            justifyContent: 'center',
-                            width: '45%',
-                        }}
-                    >
-                        {t(isOnTeam ? 'simulator.sell' : 'simulator.buy').toUpperCase()}
-                    </Button>
-                </Stack>
-            </Stack>
-        </Stack>
-    )
-}
+// const MobileRow: FC<RowProps> = ({ balance, isOnTeam, player, teamInfo, buy, sell }) => {
+//     return (
+//         <Stack alignItems={'center'} direction={'row'} spacing={1}>
+//             <Avatar
+//                 alt={player.name}
+//                 src={player.photoUrl}
+//                 sx={{ height: 100, width: 100 }}
+//                 variant={'square'}
+//             />
+//             <Stack spacing={1} width={'100%'}>
+//                 <Stack alignItems={'center'} direction={'row'} spacing={2}>
+//                     <StatusIcon status={player.statusId} />
+//                     <Stack
+//                         alignItems={'center'}
+//                         direction={'row'}
+//                         justifyContent={'space-between'}
+//                         width={'100%'}
+//                     >
+//                         <Typography fontSize={'1.2rem'}>{player.name}</Typography>
+//                         <Typography fontSize={'0.75rem'} sx={{ opacity: 0.4 }}>
+//                             {teamInfo?.name}
+//                         </Typography>
+//                     </Stack>
+//                 </Stack>
+//                 <Stack
+//                     alignItems={'center'}
+//                     direction={'row'}
+//                     justifyContent={'space-between'}
+//                     pl={1}
+//                 >
+//                     <Stack alignItems={'center'}>
+//                         <Typography fontSize={'0.8rem'} sx={{ opacity: 0.4 }}>
+//                             LAST
+//                         </Typography>
+//                         <Typography>{player.lastScore}</Typography>
+//                     </Stack>
+//                     <Stack alignItems={'center'}>
+//                         <Typography fontSize={'0.8rem'} sx={{ opacity: 0.4 }}>
+//                             AVG
+//                         </Typography>
+//                         <Typography>{player.average}</Typography>
+//                     </Stack>
+//                     <Stack alignItems={'center'}>
+//                         <Typography fontSize={'0.8rem'} sx={{ opacity: 0.4 }}>
+//                             GAMES
+//                         </Typography>
+//                         <Typography>{player.gamesPlayed}</Typography>
+//                     </Stack>
+//                 </Stack>
+//                 <Stack
+//                     alignItems={'center'}
+//                     direction={'row'}
+//                     justifyContent={'space-between'}
+//                     pl={1}
+//                 >
+//                     <Typography fontSize={'1.3rem'}>{`${t('common.cartoleta')}${
+//                         player.price
+//                     }`}</Typography>
+//                     <Button
+//                         disabled={balance - player.price < 0 && !isOnTeam}
+//                         onClick={() => (isOnTeam ? sell(player) : buy(player))}
+//                         sx={{
+//                             color: 'white',
+//                             backgroundColor: isOnTeam ? 'red' : 'green',
+//                             borderRadius: 8,
+//                             justifyContent: 'center',
+//                             width: '45%',
+//                         }}
+//                     >
+//                         {t(isOnTeam ? 'simulator.sell' : 'simulator.buy').toUpperCase()}
+//                     </Button>
+//                 </Stack>
+//             </Stack>
+//         </Stack>
+//     )
+// }
