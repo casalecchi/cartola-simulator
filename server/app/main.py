d87c7697f56e92e3e2cbbe23f983731c0bbd16b2
casalecchi <felipevilela191@gmail.com>
2025-07-20 23:09:03 -0300

    allow all domains to use backend

--- server/app/main.py
+++ server/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://*vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Métodos permitidos
     allow_headers=["*"],  # Permitir todos os headers
